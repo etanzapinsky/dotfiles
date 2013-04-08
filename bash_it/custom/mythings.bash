d46cb42806f5cd9de701dc98a65badc4ce664d5a
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-04-08 12:18:50 -0400

    moved ec2 info to nontracked file

--- bash_it/custom/mythings.bash
+++ bash_it/custom/mythings.bash
@@ -14,10 +14,3 @@ alias ec="emacsclient -nw"
 # Colorful grep
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;32'
-
-#EC2 command line tools
-export JAVA_HOME="$(/usr/libexec/java_home)"
-export EC2_KEYPAIR="etanzkey"
-export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
-export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
-export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
